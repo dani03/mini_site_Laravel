1ff90a6776d73a13eabf7310df3038e8ad29005d
dani03 <lesamessipa@gmail.com>
2020-10-26 21:23:16 +0100

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>laravel 8</title>
+</head>
+<body>
+    <h1> bonjour depuis paris...</h1>
+    <h3 class="bjr">heure locale {{ date('h:i A d/m/Y  ')}}</h3>
+
+    <footer>
+        <span>&copy; Copyright {{ date('Y')}} &middot; </span>
+    </footer>
+</body>
+</html>
