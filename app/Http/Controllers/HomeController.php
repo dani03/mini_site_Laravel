f8a9434c31363c01bda510c9cfc27a5a93d5c0a7
dani03 <lesamessipa@gmail.com>
2020-11-03 14:48:34 +0100

    remove data base

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,17 +4,17 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use App\Models\Product;
+
 
 class HomeController extends Controller
 {
     //
     public function welcome(){
-        $produits = Product::get();
+        
         // $produits = DB::table('products')
         // ->paginate(1);
         // //->orderBy('product_name', 'asc')
         // // ->get();
-        return view('pages.welcome')->with('produits', $produits);
+        return view('pages.welcome');
     }
 }
