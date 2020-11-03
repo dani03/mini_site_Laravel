f0b3df8d75ee7bdf797194f8f15f409c940ea619
dani03 <lesamessipa@gmail.com>
2020-11-03 14:04:26 +0100

    remove test page

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,9 +4,12 @@
 @section('contenu')
     <img src="/images/parisien.png" alt="parisien" class="rounded shadow-md h-32 mt-5">
     <h1 class=" text-2xl sm:text-3xl lg:text-7xl my-4 font-semibold text-indigo-700"> bonjour depuis paris...</h1>
-
+        <div class="mes_produits">
+            create by daniel.
+            {{-- {{$produits->links()}} --}}
+        </div>  
     <h3 class="bjr text-lg text-gray-700">heure locale {{ date('h:i A d/m/Y')}}</h3> 
-    <a href={{ route("vers-test") }}>page test.</a> </span> 
+    
 @endsection
 
 
